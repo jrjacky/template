5bb4a7cadc6e8431241baae4b37f829e68702fef
jrjacky <3547618505@qq.com>
2020-08-09 14:07:10 +0800

    2020.8.9

--- 图论/网络流——Dinic.cpp
+++ 图论/网络流——Dinic.cpp
@@ -1,13 +1,14 @@
-//Luogu 2756
+//Luogu 3376
 #include<iostream>
 #include<cstring>
 #include<cstdio>
 #include<queue>
 using namespace std;
 const int inf=1<<29;
-int n,m=0,s,t,tot=0,maxflow=0,head[100010],ver[100010],edge[100010],Next[100010],d[100010];
+int n,m,s,t,tot=1,head[100010],ver[100010],Next[100010],d[100010];
+long long edge[100010],maxflow;
 queue<int> q;
-void add(int x,int y,int z)
+void add(int x,int y,long long z)
 {
 	ver[++tot]=y,edge[tot]=z,Next[tot]=head[x],head[x]=tot;
 	ver[++tot]=x,edge[tot]=0,Next[tot]=head[y],head[y]=tot;
@@ -28,10 +29,10 @@ bool bfs()
 	}
 	return d[t];
 }
-int dinic(int x,int flow)
+int dinic(int x,long long flow)
 {
 	if(x==t) return flow;
-	int rest=flow,k;
+	long long rest=flow,k;
 	for(int i=head[x];i&&rest;i=Next[i]) if(edge[i]&&d[ver[i]]==d[x]+1){
 		k=dinic(ver[i],min(rest,edge[i]));
 		if(!k) d[ver[i]]=0;
@@ -43,18 +44,9 @@ int dinic(int x,int flow)
 }
 int main()
 {
-	int n1,x,y;
-	cin>>n>>n1;
-	s=0,t=n1+1,tot=1;
-	for(int i=1;i<=n;i++) add(s,i,1);
-	for(int i=n+1;i<=n1;i++) add(i,t,1);
-	while(scanf("%d%d",&x,&y)!=EOF&&x!=-1&&y!=-1){
-		add(x,y,1);
-		m++;
-	}
+	cin>>n>>m>>s>>t;
+	long long z;
+	for(int i=1,x,y;i<=m;i++) cin>>x>>y>>z,add(x,y,z);
 	while(bfs()) maxflow+=dinic(s,inf);
-	if(!maxflow){cout<<"No Solution!";return 0;}
 	cout<<maxflow<<endl;
-	for(int i=2;i<=tot;i+=2) if(ver[i]!=s&&ver[i^1]!=s&&ver[i]!=t&&ver[i^1]!=t&&edge[i^1]!=0)
-	cout<<ver[i^1]<<" "<<ver[i]<<endl;
 }
